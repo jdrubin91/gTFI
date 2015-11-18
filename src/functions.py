f26ef690ccd4610d998413e8306bd5df4e13e1e0
jdrubin91 <jd.rubin91@gmail.com>
2015-11-17 17:38:36 -0700

    updates

--- src/functions.py
+++ src/functions.py
@@ -29,7 +29,7 @@ def parse_fasta(fastafile,intervaldict):
     nucleotides = ['a','c','g','t']
     freq = [0] * 5
     fastadict = dict()
-    i=0
+    i,j,N=0,0,0
     for line in open(fastafile):
         if '>' in line[0]:
             chrom = line[1:len(line)]
