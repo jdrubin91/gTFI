8262e17340b193cbc5fe39bf7fa288895d809508
jdrubin91 <jd.rubin91@gmail.com>
2015-11-24 12:20:04 -0700

    updates

--- src/motif_distance_analyzer.py
+++ src/motif_distance_analyzer.py
@@ -23,30 +23,29 @@ def run(TFIntervaldict,pad,threshold=0.5):
                 for position in interval:
                     if position[2] != np.inf and position[2] > threshold:
                        x.append((position[0]+position[1]/2)-pad)
-            
-
-        counts,edges 	= np.histogram(x, bins=200)
-        edges 			= edges[1:]
-        X 				= np.zeros((len(counts), 2))
-        X[:,0] 			= edges
-        X[:,1] 			= counts
-        w = em.fit(X)
-        start = min(x)
-        stop = max(x)
-        sigma = np.std(x)
-        mu = np.mean(x)
-        N = len(x)
-        y = np.random.uniform(start, stop, N)
-        y = np.linspace(start,stop,N)
-        z = mu/(sigma/math.sqrt(N))
-        p = 1 - scipy.special.ndtr(z)
-        k = scipy.stats.ks_2samp(x,y)
-        m = scipy.stats.mode(x)[0][0]
-        if -0.25 < m < 0.25:
-            m = 0
-        else:
-            m = 1
-        print w,k[1]
-        distances[TF] = [w,k[1],p,m,x]
+        if len(x) > 0:
+            counts,edges 	= np.histogram(x, bins=200)
+            edges 			= edges[1:]
+            X 				= np.zeros((len(counts), 2))
+            X[:,0] 			= edges
+            X[:,1] 			= counts
+            w = em.fit(X)
+            start = min(x)
+            stop = max(x)
+            sigma = np.std(x)
+            mu = np.mean(x)
+            N = len(x)
+            y = np.random.uniform(start, stop, N)
+            y = np.linspace(start,stop,N)
+            z = mu/(sigma/math.sqrt(N))
+            p = 1 - scipy.special.ndtr(z)
+            k = scipy.stats.ks_2samp(x,y)
+            m = scipy.stats.mode(x)[0][0]
+            if -0.25 < m < 0.25:
+                m = 0
+            else:
+                m = 1
+            print w,k[1]
+            distances[TF] = [w,k[1],p,m,x]
         
     return distances
